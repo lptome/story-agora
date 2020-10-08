73d1dc7596a6be34cd90a98ebe1a26ff5db927d7
luisptc <luisptcolaco@gmail.com>
2020-10-08 17:17:05 +0100

    initial check-in of code

---
+++ StoryAgora/src/main/java/com/storyagora/StoryAgoraApplication.java
@@ -0,0 +1,13 @@
+package com.storyagora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryAgoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryAgoraApplication.class, args);
+	}
+
+}
---
+++ StoryAgora/src/test/java/com/storyagora/StoryAgoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storyagora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryAgoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
