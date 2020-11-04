3b2fb6f6b50adb9e168c29850ad50c2dd12c0b99
Luis Tome <luisptcolaco@gmail.com>
2020-11-04 17:08:31 +0000

    Add ability to search for users to search functionality

--- StoryAgora/src/main/java/com/storyagora/repositories/UserRepository.java
+++ StoryAgora/src/main/java/com/storyagora/repositories/UserRepository.java
@@ -1,11 +1,16 @@
 package com.storyagora.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.storyagora.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
-	User findByUsername(String username);
+	public User findByUsername(String username);
 	
+	@Query("SELECT u from User u WHERE u.username LIKE %?1%")
+	public List<User> findByKeyword(String keyword);
 }
