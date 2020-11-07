a839be8097ceed7f4b461126ec1335ab9a09359d
Luis Tome <luisptcolaco@gmail.com>
2020-11-07 19:51:13 +0000

    Redirect to dashboard if Login template is accessed while user is logged in.

--- StoryAgora/src/main/java/com/storyagora/web/LoginController.java
+++ StoryAgora/src/main/java/com/storyagora/web/LoginController.java
@@ -1,6 +1,7 @@
 package com.storyagora.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,21 +13,26 @@ import com.storyagora.service.UserService;
 
 @Controller
 public class LoginController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@GetMapping("/login")
-	public String login() {
+	public String login(@AuthenticationPrincipal User user) {
+		
+		if (user != null) {
+			return "redirect:/dashboard";			
+		}
+		
 		return "login";
 	}
-	
+
 	@GetMapping("/register")
 	public String register(ModelMap model) {
 		model.put("user", new User());
 		return "register";
 	}
-	
+
 	@PostMapping("/register")
 	public String registerPost(@ModelAttribute User user) {
 		userService.save(user);
